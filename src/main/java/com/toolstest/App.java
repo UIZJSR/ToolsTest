28b5214c316d023edaccb4381ef37ed016dc98ff
UIZJSR <49194969+UIZJSR@users.noreply.github.com>
2019-04-04 10:44:59 +0200

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -9,6 +9,6 @@ public class App {
 
     public static void main(String[] args) {
         name ="Pepe";
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" + name);
     }
 }
